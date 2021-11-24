90a3f353e26ac3423aaa76cebfeb6d8ad9cb0fd9
Artur Dzhus <asasin.lol10@gmail.com>
2021-11-24 11:40:13 +0200

    - Second version sidebar (height = 100vh)

--- src/TodoListSidebar/TodoListSidebar.js
+++ src/TodoListSidebar/TodoListSidebar.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react";
-import {Menu, MenuItem, ProSidebar, SidebarContent, SidebarHeader} from "react-pro-sidebar";
+import {Menu, MenuItem, ProSidebar, SidebarContent, SidebarFooter, SidebarHeader} from "react-pro-sidebar";
 import 'react-pro-sidebar/dist/css/styles.css';
-/*import TaskListApi from '../Api/TaskListApi'
-TaskListApi.getOpenTasks().then(r => console.log(r));*/
 import "./TodoListSidebar.css";
+import TaskListApi from '../Api/TaskListApi'
+
+
+TaskListApi.getOpenTasks().then(r => console.log(r));
 
 
 class TodoListSidebar extends Component
@@ -11,19 +13,21 @@ class TodoListSidebar extends Component
     render()
     {
         return(
-        <ProSidebar collapsed={false}>
-            <SidebarHeader class = 'pro-sidebar-header'>
-                <div>
-                    <p>Task Lists</p>
-                </div>
-            </SidebarHeader>
-            <SidebarContent>
-                <Menu iconShape="square">
-                    <MenuItem>Home</MenuItem>
-                    <MenuItem>Category</MenuItem>
-                </Menu>
-            </SidebarContent>
-        </ProSidebar>
+                <ProSidebar collapsed={false}>
+                    <SidebarHeader class = 'pro-sidebar-header'>
+                        <div>
+                            <p>Task Lists</p>
+                        </div>
+                    </SidebarHeader>
+                    <SidebarContent>
+                        <Menu iconShape="square">
+                            <MenuItem>Home</MenuItem>
+                            <MenuItem>Category</MenuItem>
+                        </Menu>
+                    </SidebarContent>
+                    <SidebarFooter>
+                    </SidebarFooter>
+                </ProSidebar>
         )
     }
 
