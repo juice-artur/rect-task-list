149ee509bb3cf8407929e3a7fbe62f060936c9a3
Artur Dzhus <asasin.lol10@gmail.com>
2021-11-24 13:20:11 +0200

    -sidebar v3

--- src/TodoListSidebar/TodoListSidebar.js
+++ src/TodoListSidebar/TodoListSidebar.js
@@ -2,32 +2,28 @@ import React, {Component} from "react";
 import {Menu, MenuItem, ProSidebar, SidebarContent, SidebarFooter, SidebarHeader} from "react-pro-sidebar";
 import 'react-pro-sidebar/dist/css/styles.css';
 import "./TodoListSidebar.css";
-import TaskListApi from '../Api/TaskListApi'
 
-
-TaskListApi.getOpenTasks().then(r => console.log(r));
-
-
-class TodoListSidebar extends Component
-{
-    render()
-    {
-        return(
-                <ProSidebar collapsed={false}>
-                    <SidebarHeader class = 'pro-sidebar-header'>
-                        <div>
-                            <p>Task Lists</p>
-                        </div>
-                    </SidebarHeader>
-                    <SidebarContent>
-                        <Menu iconShape="square">
-                            <MenuItem>Home</MenuItem>
-                            <MenuItem>Category</MenuItem>
-                        </Menu>
-                    </SidebarContent>
-                    <SidebarFooter>
-                    </SidebarFooter>
-                </ProSidebar>
+class TodoListSidebar extends Component {
+    constructor() {
+        super();
+        this.state = {title: "title"}
+    }
+    render() {
+        return (
+            <ProSidebar collapsed={false}>
+                <SidebarHeader class='pro-sidebar-header'>
+                    <div>
+                        <p>Task Lists</p>
+                    </div>
+                </SidebarHeader>
+                <SidebarContent>
+                    <Menu iconShape="square">
+                        <MenuItem>{this.state.title}</MenuItem>
+                    </Menu>
+                </SidebarContent>
+                <SidebarFooter>
+                </SidebarFooter>
+            </ProSidebar>
         )
     }
 
