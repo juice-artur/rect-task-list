1a8baf45f2a097d75809fbfe6eb79ebfc9b8e692
Artur Dzhus <asasin.lol10@gmail.com>
2021-11-29 17:38:13 +0200

    -Remove comments in code

--- src/App.js
+++ src/App.js
@@ -5,59 +5,6 @@ import TodoListSidebar from "./TodoListSidebar/TodoListSidebar";
 import TodoListPage from "./Pages/TodoListPage/TodoListPage";
 import TodayTasksPage from "./Pages/TodayTasksPage/TodayTasksPage";
 function App() {
-    /*const [lists, setLists] = useState([]);
-    const [currentListTask, setCurrentListTask] = useState([]);
-    const [currentListId, setCurrentListId] = useState();
-    
-    
-    useEffect(() => {
-        taskApi.getDashboard().then((data) => setLists(data.lists));
-        console.log("seEffect")
-    }, []);
-    
-    
-    let changeCurrentListTask= (listId, id) => {
-        console.log("changeCurrentListTask"+ JSON.stringify(id));
-        taskApi.getOpenTasks(listId).then((data) => setCurrentListTask(data));
-        setCurrentListId(id);
-    }
-    
-    let changeState = (task) => {
-        console.log(currentListId);
-        taskApi.patchTask(task, currentListId)
-            .then(() => taskApi.getOpenTasks(currentListId)
-                .then((data) => setCurrentListTask(data)))
-            .then(() => setCurrentListId(currentListId))
-            .then(() => taskApi.getDashboard()
-                .then((data) => setLists(data.lists)));
-    }
-
-    let deleteTask= (taskId) => {
-        console.log("deleteTask");
-        console.log(taskId);
-        console.log(currentListId);
-        taskApi.deleteTask(taskId)
-            .then(() => taskApi.getOpenTasks(currentListId)
-                .then((data) => setCurrentListTask(data)))
-            .then(() => setCurrentListId(currentListId))
-            .then(() => taskApi.getDashboard()
-                .then((data) => setLists(data.lists)));
-    }
-    
-
-    let addTask= (task) => {
-        console.log("id is: "+ currentListId);
-        taskApi.createTask(task, currentListId)
-            .then(response => response.json())
-            .then(task__ => taskApi.getOpenTasks(task__.taskListId)
-                .then((data) => setCurrentListTask(data)))
-            .then(() => setCurrentListId(currentListId))
-            .then(() => taskApi.getDashboard()
-                .then((data) => setLists(data.lists)));
-        
-    }*/
-    
-    
   return (
     <div className="App">
         <div className="main-info">
