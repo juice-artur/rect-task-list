e0e0faf9dd1409252714e667f9aaf3015139824a
Artur Dzhus <asasin.lol10@gmail.com>
2021-12-01 16:07:07 +0200

    Correct work show task

--- src/Pages/TodoListPage/TodoListPage.jsx
+++ src/Pages/TodoListPage/TodoListPage.jsx
@@ -10,31 +10,27 @@ import {useDispatch, useSelector} from "react-redux";
 const TodoListPage = () => {
     
     const params = useParams();
-    const [tasks, setTasks] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
 
     const dispatch = useDispatch();
     useEffect(()=> dispatch(loadTasks(params.id, isOpen)), [dispatch, params.id, isOpen]);
-    const tt= useSelector(state => state.tasks.tasks)
+    const tasks= useSelector(state => state.tasks.tasks)
     
-    useEffect(() => {
-        taskApi.getOpenTasks(params.id, isOpen).then(setTasks);
-    }, [params.id, isOpen]);
 
     let deleteTask= (taskId) => {
         taskApi.deleteTask(taskId)
-            .then(() =>setTasks(tasks.filter(t => t.id !== taskId)));
+            /*.then(() =>setTasks(tasks.filter(t => t.id !== taskId)));*/
     }
 
     let changeState = (task) => {
         taskApi.patchTask(task)
             .then(() => tasks.map(_task => task.id === _task.id ? _task = task : _task))
-                .then(setTasks);
+                /*.then(setTasks);*/
     }
 
 
     let addTask= (task) => {
-        taskApi.createTask(task, params.id).then(task => setTasks([task, ...tasks]));
+        taskApi.createTask(task, params.id).then(task => console.log("1111")/*setTasks([task, ...tasks])*/);
 
     }
     
