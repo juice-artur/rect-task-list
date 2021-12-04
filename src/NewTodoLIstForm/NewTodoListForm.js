1cf65a040aa9cf803d6ec8160a93e8a941e0ac16
asasin.lol10@gmail.com <JJ-STU_DIO[D[D[-[C[C[C>
2021-12-04 21:06:15 +0200

    ----------

--- src/NewTodoLIstForm/NewTodoListForm.js
+++ src/NewTodoLIstForm/NewTodoListForm.js
@@ -1,7 +1,9 @@
 ﻿import {useDispatch} from "react-redux";
 import {addTaskList} from "../store/dashboard/addTaskListAction";
 import "./NewTodoListForm.css"
+
 const NewTodoListForm = () => {
+    
     const dispatch = useDispatch();
 
     const onClickAdd = (event) => {
@@ -10,7 +12,7 @@ const NewTodoListForm = () => {
     };
 
     return (
-        <form onSubmit={onClickAdd} name="taskForm" id="addForm">
+        <form onSubmit={onClickAdd}  name="todo-list-Form" id="addForm">
             <input className='item' type="text" name="title"  placeholder="Enter list title" required/>
             <button className='add-list-button'  type="submit">Add list</button>
         </form>
